7ffff4b2060a1a212ad1e91c4e0050775d9a1bc5
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-01-19 20:05:19 +0100

    Changes code style

--- test/testInputs.js
+++ test/testInputs.js
@@ -1,27 +1,29 @@
 'use strict';
 
-var assert = require('assert')
-  , ntr = require('../index')
-  ;
+var assert = require('assert');
+var ntr = require('../index');
 
 describe('norsk tipping results', function(){
 
   it('requires type to be defined', function(done){
 
     var opts = {
-        type:false
-      }
-      ;
+      type:false
+    };
 
     ntr(opts, function(err, data){
       assert.throws(function(){
-          if(err) throw err;
+          if(err){
+            throw err;
+          } else {
+            console.log(data);
+          }
         }, function(err){
           if((err instanceof Error) && /Missing required param: type/.test(err)){
-            return true
+            return true;
           }
         },
-        "Unexpected error"
+        'Unexpected error'
       );
       done();
     });
